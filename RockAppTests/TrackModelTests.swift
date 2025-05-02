9797842c61f64b16ae047699952699c430e7e388
Pradeep Dahiya <pradeepdahiya@hotmail.com>
2025-05-02 16:51:40 +0100

    Added unit tests for Track model decoding

--- RockAppTests/TrackModelTests.swift
+++ RockAppTests/TrackModelTests.swift
@@ -6,30 +6,29 @@
 //
 
 import XCTest
-
+@testable import RockApp
 final class TrackModelTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testTrackDecoding() throws {
+        let json = """
+        {
+            "trackName": "I'm Yours",
+            "artistName": "Jason Mra",
+            "trackPrice": 1.29,
+            "artworkUrl100": "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/f8/ac/ec/f8acec21-40e2-fa91-fe61-1be3cbc1467a/mzi.jokpbtnf.jpg/100x100bb.jpg",
+            "trackTimeMillis": 242947,
+            "releaseDate": "2008-02-12T08:00:00Z",
+            "trackViewUrl": "https://music.apple.com/us/album/im-yours/277635758?i=277635828&uo=4"
         }
-    }
+        """.data(using: .utf8)!
 
+        let track = try JSONDecoder().decode(Track.self, from: json)
+        XCTAssertEqual(track.trackName, "I'm Yours")
+        XCTAssertEqual(track.artistName, "Jason Mra")
+        XCTAssertEqual(track.trackPrice, 1.29)
+        XCTAssertEqual(track.artworkUrl100, "https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/f8/ac/ec/f8acec21-40e2-fa91-fe61-1be3cbc1467a/mzi.jokpbtnf.jpg/100x100bb.jpg")
+        XCTAssertEqual(track.trackTimeMillis, 242947)
+        XCTAssertEqual(track.releaseDate, "2008-02-12T08:00:00Z")
+        XCTAssertEqual(track.trackViewUrl, "https://music.apple.com/us/album/im-yours/277635758?i=277635828&uo=4")
+    }
 }
