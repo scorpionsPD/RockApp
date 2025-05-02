5f9edc73815cf059b1e468ff5283dc4d1f2de38b
Pradeep Dahiya <pradeepdahiya@hotmail.com>
2025-05-02 14:17:37 +0100

    Added error handling

--- RockApp/Networking/TrackService.swift
+++ RockApp/Networking/TrackService.swift
@@ -7,14 +7,45 @@
 
 import Foundation
 
+enum NetworkError: Error, LocalizedError {
+    case badURL
+    case badResponse
+    case decodingError
+    case unknown
+
+    var errorDescription: String? {
+        switch self {
+        case .badURL:
+            return "Invalid URL."
+        case .badResponse:
+            return "Server returned a bad response."
+        case .decodingError:
+            return "Failed to decode data."
+        case .unknown:
+            return "An unknown error occurred."
+        }
+    }
+}
+
 class TrackService {
     func fetchTracks() async throws -> [Track] {
         guard let url = URL(string: "https://itunes.apple.com/search?term=rock") else {
-            throw URLError(.badURL)
+            throw NetworkError.badURL
         }
-        
-        let (data, _) = try await URLSession.shared.data(from: url)
-        let response = try JSONDecoder().decode(SearchResponse.self, from: data)
-        return response.results
+
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url)
+
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                throw NetworkError.badResponse
+            }
+
+            let decodedResponse = try JSONDecoder().decode(SearchResponse.self, from: data)
+            return decodedResponse.results
+        } catch is DecodingError {
+            throw NetworkError.decodingError
+        } catch {
+            throw NetworkError.unknown
+        }
     }
 }
